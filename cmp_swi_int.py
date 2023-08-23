f14c68c86d812589a026ae6b463ac34234063145
benjamin <benjamin.hien@web.de>
2023-08-23 16:53:34 +0200

    power calculation

--- cmp_swi_int.py
+++ cmp_swi_int.py
@@ -4,7 +4,36 @@ cells = parse()
 
 cell = cells['AND2x2_ASAP7_75t_R']
 
-for key in cell.keys():
-    print(cell[key])
+def cmp_avg_cell_internal(cell):
+    avg_cell_internal = 0
+    counter = 0
+    for pin in cell.keys():
+        #print(pin)
+        if pin != 'Y':
+            for value_vector in cell[pin]:
+                #print(value_vector)
+                for value in value_vector:
+                    avg_cell_internal += abs(value)
+                    counter += 1
+
+    avg_cell_internal /= counter
+    print(avg_cell_internal)
+
+def cmp_avg_net_switching(cell):
+    avg_net_switching = 0
+    counter = 0
+    for pin in cell.keys():
+        if pin == 'Y':
+            for value_vector in cell[pin]:
+                for value in value_vector:
+                    avg_net_switching += abs(value)
+                    counter += 1
+
+    avg_net_switching /= counter
+    print(avg_net_switching)
+
+cmp_avg_cell_internal(cell)
+cmp_avg_net_switching(cell)
+
 
 #print(cell)
